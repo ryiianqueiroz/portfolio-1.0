2acf19b5af923c8a8ebf201001cff703c795994d
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-03-28 21:36:14 -0300

    about me update scss and images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,37 @@
 import "./About.css"
+import React from "../../assets/react-colorido.svg";
+import Sass from "../../assets/sass-colorido.svg";
+import Tailwind from "../../assets/tailwind-colorido.svg";
+import Angular from "../../assets/angular-colorido.svg";
+import CSS from "../../assets/css-colorido.svg";
+import HTML from "../../assets/html-colorido.png";
+import Javascript from "../../assets/javascript-colorido.svg";
 
 function About() {
     return (
       <section>
-        <h1>Sobre Eu</h1>
+        <h1 id="about-me-h1">Sobre Eu</h1>
       
         <div className="about-me">
           <div className="text-about-me">
-            <p>Originário de Fortaleza, no Ceará, mergulhei no empolgante mundo da programação quando ingressei no Instituto Federal de Educação, Ciência e Tecnologia do Ceará, onde atualmente curso o 5 * Semestre de Engenharia da Computação.</p>
-            
-            <p>Sou um profissional dedicado, com experiência sólida em desenvolvimento Front-End 
-              utilizando as mais recentes tecnologias, com foco especial em React.
-            </p>
-
+            <p>Natural de Fortaleza, no Ceará, minha jornada no fascinante mundo da programação começou quando iniciei meu curso no Instituto Federal de Educação, Ciência e Tecnologia do Ceará. Atualmente, estou no 5º semestre do curso de Engenharia da Computação.</p>
             <p>No momento, estou trabalhando em diversos projetos para aprimorar minhas habilidades e constantemente explorando novas tecnologias em demanda no mercado.</p>
+
+            <h2>Tecnologias Usadas:</h2>
+            <div className="tecnology-used">
+              <img src={React} alt="#" />
+              <img src={Angular} alt="#" />
+              <img src={Sass} alt="#" />
+              <img src={Tailwind} alt="#" />
+              <img src={HTML} alt="#" />
+              <img src={CSS} alt="#" />
+              <img src={Javascript} alt="#" />
+            </div>
+
           </div>
-          <div className="photo-me"></div>
+          <div className="photo-me">
+            <div className="my-photo"></div>
+          </div>
         </div>
       </section>
     );
