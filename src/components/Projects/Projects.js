e5dfc5b2ce39d7e3d83ba2ad8a4fa71395f94828
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-04-01 11:08:19 -0300

    other projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -37,10 +37,10 @@ function Projects() {
               </div>
 
               <div className="links-project">
-                <a href="#">
+                <a href="https://github.com/ryiianqueiroz/insure-react-project">
                   <button><img src={GitHub} alt="#" /> Código</button>
                 </a>
-                <a href="#">
+                <a href="https://ryiianqueiroz.github.io/insure-react-project/">
                   <button><img id="open-link" src={OpenLink} alt="#" />Veja</button>
                 </a>
               </div>
@@ -68,10 +68,10 @@ function Projects() {
               </div>
 
               <div className="links-project">
-                <a href="#">
+                <a href="https://github.com/ryiianqueiroz/tip-calculator-react">
                   <button><img src={GitHub} alt="#" /> Código</button>
                 </a>
-                <a href="#">
+                <a href="https://ryiianqueiroz.github.io/tip-calculator-react/">
                   <button><img id="open-link" src={OpenLink} alt="#" />Veja</button>
                 </a>
               </div>
@@ -99,15 +99,145 @@ function Projects() {
               </div>
 
               <div className="links-project">
-                <a href="#">
+                <a href="https://github.com/ryiianqueiroz/react-sunnyside-responsive">
                   <button><img src={GitHub} alt="#" /> Código</button>
                 </a>
-                <a href="#">
+                <a href="https://ryiianqueiroz.github.io/react-sunnyside-responsive/">
                   <button><img id="open-link" src={OpenLink} alt="#" />Veja</button>
                 </a>
               </div>
             </div>
 
+          </div>
+
+        </div>
+
+        <div className="other-projects">
+          
+          <div className="ot-project">
+            <div className="links-ot-project">
+              <hr />
+
+              <div className="links-ot">
+                <a href="https://github.com/ryiianqueiroz/advice-generator-api-react"><img src={GitHub} alt="#" id="git-icon" /></a>
+                <a href="https://ryiianqueiroz.github.io/advice-generator-api-react/"><img src={OpenLink} alt="#" id="link-icon" /></a>
+              </div>
+            </div>
+
+            <h1>Advice</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem odit at ab explicabo! Tenetur consectetur exercitationem modi iste harum, porro beatae perspiciatis omnis aut sequi nemo earum ut numquam?</p>
+            <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+            </div>
+          </div>
+
+          <div className="ot-project">
+            <div className="links-ot-project">
+              <hr />
+
+              <div className="links-ot">
+                <a href="https://github.com/ryiianqueiroz/password-generator"><img src={GitHub} alt="#" id="git-icon" /></a>
+                <a href="https://ryiianqueiroz.github.io/password-generator/"><img src={OpenLink} alt="#" id="link-icon" /></a>
+              </div>
+            </div>
+
+            <h1>Senha Forte</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem odit at ab explicabo! Tenetur consectetur exercitationem modi iste harum, porro beatae perspiciatis omnis aut sequi nemo earum ut numquam?</p>
+            <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+            </div>
+          </div>
+
+          <div className="ot-project">
+            <div className="links-ot-project">
+              <hr />
+
+              <div className="links-ot">
+                <a href="https://github.com/ryiianqueiroz/loopstudios-react-project"><img src={GitHub} alt="#" id="git-icon" /></a>
+                <a href="https://ryiianqueiroz.github.io/loopstudios-react-project/"><img src={OpenLink} alt="#" id="link-icon" /></a>
+              </div>
+            </div>
+
+            <h1>loopstudios</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem odit at ab explicabo! Tenetur consectetur exercitationem modi iste harum, porro beatae perspiciatis omnis aut sequi nemo earum ut numquam?</p>
+            <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+            </div>
+          </div>
+
+          <div className="ot-project">
+            <div className="links-ot-project">
+              <hr />
+
+              <div className="links-ot">
+                <a href="https://github.com/ryiianqueiroz/snap-tailwind"><img src={GitHub} alt="#" id="git-icon"/></a>
+                <a href="https://ryiianqueiroz.github.io/snap-tailwind/"><img src={OpenLink} alt="#" id="link-icon"/></a>
+              </div>
+            </div>
+
+            <h1>SNAP</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem odit at ab explicabo! Tenetur consectetur exercitationem modi iste harum, porro beatae perspiciatis omnis aut sequi nemo earum ut numquam?</p>
+            <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+            </div>
+          </div>
+
+          <div className="ot-project">
+            <div className="links-ot-project">
+              <hr />
+
+              <div className="links-ot">
+                <a href="https://github.com/ryiianqueiroz/react-pricing-component"><img src={GitHub} alt="#" id="git-icon" /></a>
+                <a href="https://ryiianqueiroz.github.io/react-pricing-component/"><img src={OpenLink} alt="#" id="link-icon" /></a>
+              </div>
+            </div>
+
+            <h1>Tabela de Preços</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem odit at ab explicabo! Tenetur consectetur exercitationem modi iste harum, porro beatae perspiciatis omnis aut sequi nemo earum ut numquam?</p>
+            <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+            </div>
+          </div>
+
+          <div className="ot-project">
+            <div className="links-ot-project">
+              <hr />
+
+              <div className="links-ot">
+                <a href="https://github.com/ryiianqueiroz/social-media-dashboard"><img src={GitHub} alt="#" id="git-icon" /></a>
+                <a href="https://ryiianqueiroz.github.io/social-media-dashboard/"><img src={OpenLink} alt="#" id="link-icon" /></a>
+              </div>
+            </div>
+
+            <h1>Social Media</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem odit at ab explicabo! Tenetur consectetur exercitationem modi iste harum, porro beatae perspiciatis omnis aut sequi nemo earum ut numquam?</p>
+            <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+            </div>
           </div>
 
         </div>
