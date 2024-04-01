c8f8bd0798de5263035aaf9121a3b1dc0bd3f54f
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-03-31 22:55:09 -0300

    projects updates and icons

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,12 @@ import Insure from "../../assets/desktop-designs/insure-design.jpg"
 import TipCalculator from "../../assets/desktop-designs/tip-calculator-design.jpg"
 import Sunnyside from "../../assets/desktop-designs/sunnyside-design.jpg"
 import GitHub from "../../assets/github-logo-white.png"
+import OpenLink from "../../assets/open-link-icon.png"
+import React from "../../assets/react-colorido.svg"
+import SASS from "../../assets/sass-colorido.svg"
+import HTML from "../../assets/html-colorido.png"
+import CSS from "../../assets/css-colorido.svg"
+import JS from "../../assets/javascript-colorido.svg"
 
 function Projects() {
     return (
@@ -21,12 +27,21 @@ function Projects() {
               <h1>Insure</h1>
               <hr />
               <p id="p-info-project">Desenvolvimento de um site corporativo com uma interface elegante e intuitiva.</p>
+
+              <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+              </div>
+
               <div className="links-project">
                 <a href="#">
-                  <button><img src={GitHub} alt="#" /></button>
+                  <button><img src={GitHub} alt="#" /> Código</button>
                 </a>
                 <a href="#">
-                  <button>Acessar Projeto</button>
+                  <button><img id="open-link" src={OpenLink} alt="#" />Veja</button>
                 </a>
               </div>
             </div>
@@ -43,12 +58,21 @@ function Projects() {
               <h1>Splitter</h1>
               <hr />
               <p id="p-info-project">Calcule a contribuição individual necessária da mesa para cobrir a conta.</p>
+
+              <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+              </div>
+
               <div className="links-project">
                 <a href="#">
-                  <button><img src={GitHub} alt="#" /></button>
+                  <button><img src={GitHub} alt="#" /> Código</button>
                 </a>
                 <a href="#">
-                  <button>Acessar Projeto</button>
+                  <button><img id="open-link" src={OpenLink} alt="#" />Veja</button>
                 </a>
               </div>
             </div>
@@ -65,12 +89,21 @@ function Projects() {
               <h1>Sunnyside</h1>
               <hr />
               <p id="p-info-project">Criação de uma landing page responsiva do desafio oferecido pelo Front End Mentor.</p>
+              
+              <div className="icon-skills">
+                <img src={React} alt="react-icon" />
+                <img src={SASS} alt="sass-icon" />
+                <img src={HTML} alt="html-icon" />
+                <img src={CSS} alt="css-icon" />
+                <img src={JS} alt="js-icon" />
+              </div>
+
               <div className="links-project">
                 <a href="#">
-                  <button><img src={GitHub} alt="#" /></button>
+                  <button><img src={GitHub} alt="#" /> Código</button>
                 </a>
                 <a href="#">
-                  <button>Acessar Projeto</button>
+                  <button><img id="open-link" src={OpenLink} alt="#" />Veja</button>
                 </a>
               </div>
             </div>
