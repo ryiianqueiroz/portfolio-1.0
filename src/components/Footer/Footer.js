dfe07ebe70e1fffa92eb9d77134da2a608aec8ca
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-04-04 15:38:19 -0300

    copy link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,42 @@
 import "./Footer.css"
+import { useRef, useEffect } from "react"
 import GitHub from "../../assets/github-logo-white.png"
 import Instagram from "../../assets/instagram-logo-white.png"
 import Linkedin from "../../assets/linkedin-white-logo.png"
 import Email from "../../assets/email-icon-white.png"
 
 function Footer() {
+
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const ativarClick = (event) => {
+            let r = document.querySelector(':root');
+            r.style.setProperty('--conteudo', '"Copiado"');
+        };
+        
+        const element = ref.current;
+
+        element.addEventListener('click', ativarClick);
+      
+        return () => {
+          element.removeEventListener('click', ativarClick);
+        };
+    }, []);
+
+    const tirarClick = () => {
+        let r = document.querySelector(':root');
+        r.style.setProperty('--conteudo', '"Copiar"');
+    }
+
     return (
         <footer>
             <div className="email-me">
                 <h1>Interesado?</h1>
                 <h1>Entre em Contato!</h1>
                 <p>Meu email profissional estará sempre aberto para novas oportunidades!</p>
-                <div className="btn-email">
+                
+                <div className="btn-email" ref={ref} onMouseLeave={tirarClick}>
                     <div className="email-img">
                         <img src={Email} alt="email-icon" />
                     </div>
