705297928c6a0c805c0104e5cb6b315f1fad2d4d
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-04-06 15:40:26 -0300

    add menu ham and animations

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,10 +7,25 @@ function Nav() {
         top: quant,
         behavior: 'smooth'
       })
-    }  
+  }  
+
+  let anteriorScroll = window.scrollY;
+
+  window.onscroll = function() {
+    let currentScroll = window.scrollY;
+    
+    if (anteriorScroll > currentScroll) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-150px";
+    }
+
+    anteriorScroll = currentScroll;
+  }
   
     return (
-      <nav>
+      <nav id="navbar">
+        
         <div className="r-flex">
           <span className="r-circle">R</span>
         </div>
@@ -20,8 +35,16 @@ function Nav() {
           <li onClick={() => scrollToBottom(570)}>Sobre Eu</li>
           <li onClick={() => scrollToBottom(1100)}>Projetos</li>
           <li onClick={() => scrollToBottom(4000)}>Contato</li>
-          <li><button>Currículo</button></li>
+          <li id="cv"><button>Currículo</button></li>
         </ul>
+
+        <input type="checkbox" id="ham" />
+
+        <label htmlFor="ham" className="hamburger">
+          <div id="s1"></div>
+          <div id="s2"></div>
+          <div id="s3"></div>
+        </label>
         
       </nav>
     );
