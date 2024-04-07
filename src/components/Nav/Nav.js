df5cc618ba35b4fc77db367ebf04c6c8f1898ca3
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-04-07 18:00:20 -0300

    add nav responsive

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -22,6 +22,17 @@ function Nav() {
 
     anteriorScroll = currentScroll;
   }
+
+  function checkElement () {
+    const ul = document.querySelector("ul")
+
+    if ( ul.classList.contains("ul-toggle") ) {
+      ul.classList.remove("ul-toggle")
+      document.getElementById("ham").checked = false;
+    } else {
+      ul.classList.add("ul-toggle")
+    }
+  }
   
     return (
       <nav id="navbar">
@@ -31,14 +42,14 @@ function Nav() {
         </div>
 
         <ul>
-          <li onClick={() => scrollToBottom(0)}>Home</li>
-          <li onClick={() => scrollToBottom(570)}>Sobre Eu</li>
-          <li onClick={() => scrollToBottom(1100)}>Projetos</li>
-          <li onClick={() => scrollToBottom(4000)}>Contato</li>
+          <li onClick={() => [scrollToBottom(0), checkElement()]}>Home</li>
+          <li onClick={() => [scrollToBottom(570), checkElement()]}>Sobre Eu</li>
+          <li onClick={() => [scrollToBottom(1100), checkElement()]}>Projetos</li>
+          <li onClick={() => [scrollToBottom(4000), checkElement()]}>Contato</li>
           <li id="cv"><button>Currículo</button></li>
         </ul>
 
-        <input type="checkbox" id="ham" />
+        <input type="checkbox" id="ham" onClick={checkElement}/>
 
         <label htmlFor="ham" className="hamburger">
           <div id="s1"></div>
